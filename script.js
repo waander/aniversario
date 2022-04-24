d449b28d2eadce27b82f92c5e75cab8755464bcc
Wander Souza <waander.souza@outlook.com>
2022-04-23 21:43:33 -0300

    Update

--- script.js
+++ script.js
@@ -1,75 +1,52 @@
-nome = window.prompt('Qual seu nome?')
-dataNasc = window.prompt('Qual a data do seu nascimento?' , 'DD/MM/AAAA')
+//nome = window.prompt('Qual seu nome?')
+//dataNasc = window.prompt('Qual a data do seu nascimento?' , 'DD/MM/AAAA')
+nome = 'Wander de Souza'
+dataNasc = '23/06/1993'
+
+var dataAtual = new Date();
+var anoAtual = dataAtual.getFullYear();
+var anoNascParts = dataNasc.split('/');
+var diaNasc =anoNascParts[0];
+var mesNasc =anoNascParts[1];
+var anoNasc =anoNascParts[2];
+var idade = anoAtual - anoNasc;
+var mesAtual = dataAtual.getMonth() + 1;
 
 function calculaIdade(dataNasc){ 
 
-    var dataAtual = new Date();
-    var anoAtual = dataAtual.getFullYear();
-    var anoNascParts = dataNasc.split('/');
-    var diaNasc =anoNascParts[0];
-    var mesNasc =anoNascParts[1];
-    var anoNasc =anoNascParts[2];
-    var idade = anoAtual - anoNasc;
-    var mesAtual = dataAtual.getMonth() + 1; 
+//Se mês atual for menor que o nascimento, não fez aniversário ainda;  
 
-    //Se mês atual for menor que o nascimento, não fez aniversário ainda;  
-
-    if(mesAtual < mesNasc){
-    idade--; 
+  if(mesAtual < mesNasc){ 
+    idade--; //Se estiver no mês do nascimento, verificar o dia;
     } else {
 
-    //Se estiver no mês do nascimento, verificar o dia;
-
-    if(mesAtual == mesNasc){ 
-    if(new Date().getDate() < diaNasc ){ 
-        
-    //Se a data atual for menor que o dia de nascimento, ainda não fez aniversário
-
-    idade--; 
-    }
-    }
-    } 
-    return idade; 
-   }
-
-   console.log(calculaIdade(dataNasc));
-
-    var dataAtual = new Date();
-    var anoAtual = dataAtual.getFullYear();
-    var anoNascParts = dataNasc.split('/');
-    var diaNasc =anoNascParts[0];
-    var mesNasc =anoNascParts[1];
-    var anoNasc =anoNascParts[2];
-    var idade = anoAtual - anoNasc;
-    var mesAtual = dataAtual.getMonth() + 1; 
-
-   var idadeF = (calculaIdade(dataNasc))
-
-   document.write('<header>Olá, ' + nome + ' você tem ' + (calculaIdade(dataNasc)) + ' anos.</header>');
 
 
-   document.write('<img src="imagens/bolo.png" alt="" srcset="">')
-   // -------------------------------------------------------------------------------------------------------------------------
-   var proxAniv = (diaNasc + '/' + mesNasc + '/'+ (Number (anoNasc) + Number (idadeF) + 1 ));
+      if(mesAtual == mesNasc){ 
+        if(new Date().getDate() < diaNasc ){  //Se a data atual for menor que o dia de nascimento, ainda não fez aniversário.;
 
-// Precisamos quebrar a string para retornar cada parte
+          idade--; 
+          }
+        }
+      } 
+      return idade; 
+      }
+      
+var idadeF = (calculaIdade(dataNasc))
+var proxAniv = (diaNasc + '/' + mesNasc + '/'+ (Number (anoNasc) + Number (idadeF) + 1 ));
+
+// Quebraando a string para retornar cada parte.;
 var proxAnivParts = proxAniv.split('/');
-
 var diaProxAniv = proxAnivParts[0];
 var mesProxAniv = proxAnivParts[1];
 var anoProxAniv = proxAnivParts[2];
 
-// Agora podemos inicializar o objeto Date, lembrando que o mês começa em 0, então fazemos -1.
+//Inicializando o objeto Date, lembrando que o mês começa em 0, então fazemos -1.;
+
 proxAnivF = new Date(anoProxAniv, mesProxAniv - 1, diaProxAniv);
 
 const diff = Math.abs(dataAtual.getTime() - proxAnivF.getTime());
-
 const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-document.write(' <div class=fDays> Faltam ' + days + ' dias para o seu aniversário. </div>');
-
-
-
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -86,4 +63,7 @@ function countDown() {
   document.getElementById('hours').innerText = Math.floor(diff % day / hour);
   document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
   document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
-}
+
+  document.getElementById('nome').innerText= nome;
+  document.getElementById('idade').innerText= idadeF;
+  }
